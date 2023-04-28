f6909d3e1fbb34e182243da3c8c86f5ebb54ae38
Ivan Maksimov <jkm182@gmail.com>
2023-04-28 23:02:30 +0300

    first bot version 0.01

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("yes")
+from aiogram import Bot, Dispatcher, executor, types
+from config import API
+
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    invite_message = "Здравствуйте, вас приветствует бот из Атлашево!\nБудем рады вашему отзыву о нашей продукции!\nДля этого нужно нажать кнопку *Оставить отзыв*"
+    await message.answer(
+        invite_message, parse_mode="markdown"
+    )  # отвечает без комментария
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
