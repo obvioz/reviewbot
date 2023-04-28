2fef75ddcb40c5ae51b00a054d09b39ccf97af5c
Ivan Strelkov <i.strelkov@yadro.com>
2023-04-29 00:07:34 +0300

    mypy fixed

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from aiogram import Bot, Dispatcher, executor, types
-from config import API
-
 import logging
 
+from aiogram import Bot, Dispatcher, executor, types
+
+from config import API
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API)
 dp = Dispatcher(bot)
 
 
+WELCOME_MESSAGE = """Здравствуйте, вас приветствует бот из Атлашево!
+Будем рады вашему отзыву о нашей продукции!
+Для этого нужно нажать кнопку *Оставить отзыв*"""
+
+
 @dp.message_handler(commands=["start"])
 async def send_welcome(message: types.Message):
-    invite_message = "Здравствуйте, вас приветствует бот из Атлашево!\nБудем рады вашему отзыву о нашей продукции!\nДля этого нужно нажать кнопку *Оставить отзыв*"
+
     await message.answer(
-        invite_message, parse_mode="markdown"
+        WELCOME_MESSAGE, parse_mode="markdown"
     )  # отвечает без комментария
 
 
